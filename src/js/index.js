1d3767385c6b38c5c248b1fdafef04ee58d44e16
abstractmagicalwand <abstractmagicalwand@gmail.com>
2016-10-23 00:24:34 +0300

    tasks display

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,19 @@
 const taskDB = [{
-  'description': 'Купить молока.',
+  description: 'Купить молока.',
 }, {
-  'description': 'Выпить колы.',
+  description: 'Выпить колы.',
 }, {
-  'description': 'Познакомиться с девочкой.'
+  description: 'Познакомиться с девочкой.'
 }];
 
 const App = React.createClass({
+
   render: function() {
     return (
       <div className="app">
         <HatApp />
         <NavSideBarApp />
-        <MainApp data="{taskDB}" />
+        <MainApp />
         <AreaInputTaskApp />
       </div>
     );
@@ -36,24 +37,42 @@ const NavSideBarApp = React.createClass({
 });
 
 const MainApp = React.createClass({
-  render: function() {
-    const tasks = this.props.data.map(function(item, index) {
-      return (
-        <TrackTask key={index} data={item} />
-      )
+  getInitialState: function() {
+    return (
+      {tasks: taskDB}
+    );
+  },
+
+  propTypes: {
+    data: React.PropTypes.shape({
+      description: React.PropTypes.string.isRequired
     })
+  },
+
+  render: function() {
+    const tasks = this.state.tasks.map(function(item, index) {
+      return (
+        <TrackTask key={index} data={item['description']} />
+      )
+    });
 
     return (
-      <div className="main-app"></div>
+      <div className="main-app">{tasks}</div>
     );
   }
 });
 
 const AreaInputTaskApp = React.createClass({
+  onBtnClickHandler: function() {
+
+  },
   render: function() {
     return (
       <div className="area-input-task-app">
-        <textarea placeholder="#tags *priority @project :::deathtimer [create date]" className="text-area-input-task-app"></textarea>
+        <textarea 
+          placeholder="#tags *priority @project :::deathtimer [create date]" 
+          className="text-area-input-task-app">
+        </textarea>
         <button className="btn-area-input-task-app"></button>
       </div>
     );
@@ -63,10 +82,10 @@ const AreaInputTaskApp = React.createClass({
 const TrackTask = React.createClass({
   render: function() {
     return (
-      <div class="track-task"></div>
-    )
+      <div className="track-task">{this.props.data}</div>
+    );
   }
-})
+});
 
 ReactDOM.render(
   <App />,
