6ec970ae23ddd476e8ec432bd63b588ee5a18159
abstractmagicalwand <abstractmagicalwand@gmail.com>
2016-10-22 22:01:22 +0300

    add server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(__dirname));
+
+app.listen(app.get('port'), function() {
+  console.log('run');
+});
