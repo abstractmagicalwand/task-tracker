def0df70d7f43ae2bb3186438bb047a9fd0cf50f
abstractmagicalwand <abstractmagicalwand@gmail.com>
2016-11-19 03:11:06 +0300

    style: fix position

--- src/js/component/help.jsx
+++ src/js/component/help.jsx
@@ -12,7 +12,6 @@ export default class Help extends React.Component {
         <h3 className='help-title'>Spells</h3>
         <p><span className='help-prop'>timer death:</span> hh/mm/ss</p>
         <p><span className='help-prop'>priority:</span> * </p>
-        <p>count stars, level priority</p>
         <p><span className='help-prop'>project:</span> @name-project</p>
         <p><span className='help-prop'>tags:</span> #nameTag</p>
       </div>
